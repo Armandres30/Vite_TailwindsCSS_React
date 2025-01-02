67b63e5a28e1f655b5f6f1f16b45cf60e7e80ba3
Armando <armando@Armandos-MacBook-Pro.local>
2025-01-02 22:31:22 +0100

    Components in Navbar part 2

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,65 +1,79 @@
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    const activeStyle = 'underline'
+    
     return (
-        <nav>
-            <ul>
-                <li>
-                    armando_zax@hotmail.com
-                </li>
-                <li>
-                    <NavLink to='/my-orders'>
-                        MyOrders
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/my-account'>
-                        MyAccount
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/sign-in'>
-                        Sign in
-                    </NavLink>
-                </li>
-                
-            </ul>
-            <ul>
-                <li>
-                    <NavLink to='/'>
+        <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
+            <ul className='flex items-center gap-3'>
+                <li className='font-semibold text-lg'>
+                    <NavLink 
+                        to='/'
+                        className={({isActive}) => isActive ? activeStyle: undefined}>
                         Shopi
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/'>
+                    <NavLink to='/'
+                    className={({isActive}) => isActive ? activeStyle: undefined}>
                         All
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/clothes'>
+                    <NavLink to='/clothes'
+                    className={({isActive}) => isActive ? activeStyle: undefined}>
                         Clothes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/electronics'>
+                    <NavLink to='/electronics'
+                    className={({isActive}) => isActive ? activeStyle: undefined}>
                         Electronics
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/furnitures'>
+                    <NavLink to='/furnitures'
+                    className={({isActive}) => isActive ? activeStyle: undefined}>
                         Furnitures
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/toys'>
+                    <NavLink to='/toys'
+                    className={({isActive}) => isActive ? activeStyle: undefined}>
                         Toys
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/others'>
+                    <NavLink to='/others'
+                    className={({isActive}) => isActive ? activeStyle: undefined}>
                         Others
                     </NavLink>
                 </li>
+            </ul>
+            
+            <ul className='flex items-center gap-3'>
+                <li>
+                    armando_zax@hotmail.com
+                </li>
+                <li>
+                    <NavLink to='/my-orders'
+                        className={({isActive}) => isActive ? activeStyle: undefined}>
+                        MyOrders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/my-account'
+                        className={({isActive}) => isActive ? activeStyle: undefined}>
+                        MyAccount
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/sign-in'
+                        className={({isActive}) => isActive ? activeStyle: undefined}>
+                        Sign in
+                    </NavLink>
+                </li>
+                
             </ul>
         </nav>
     )
